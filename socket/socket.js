ac436557eddde758c63e024577e0fb0506c676e9
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2023-08-29 07:39:25 -0500

    switched to express server

--- socket/socket.js
+++ socket/socket.js
@@ -1,11 +1,17 @@
+const express = require("express")
+const { createServer } = require("http")
 const { Server } = require("socket.io")
 
-const io = new Server(3000)
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
 
 io.on("connection", (socket) => {
   console.log(`${socket.id} connected, current clients: ${io.engine.clientsCount}`)
-  // socket.on('printjobs', (jobs) => {
-  //   console.log('print jobs sent to socket:\n', jobs)
-  //   socket.broadcast.emit('printjobs',jobs)
-  // })
+  socket.on('printjobs', (jobs) => {
+    console.log('print jobs sent to socket:\n', jobs)
+    socket.broadcast.emit('printjobs',jobs)
+  })
 })
+
+httpServer.listen(3000)
