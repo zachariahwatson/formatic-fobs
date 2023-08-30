b65dfe8a85d4c3c43419394c8953a79d381eea4d
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2023-08-30 03:37:45 -0500

    renamed socket

---
+++ server/server.js
@@ -0,0 +1,75 @@
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') })
+const { createServer } = require("http")
+const { Server } = require("socket.io")
+const printer = require('./printer')
+const prisma = require('../app/utils/prisma').prisma
+const printQueue = require('./../app/utils/queue')
+
+const httpServer = createServer()
+const io = new Server(httpServer)
+printer.init()
+printer.serialWrite('M300 S440 P200')
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected, current clients: ${io.engine.clientsCount}`)
+  socket.on('printjobs', (jobs) => {
+    //console.log('print jobs sent to socket:\n', jobs)
+    socket.broadcast.emit('printjobs',jobs)
+  })
+
+  //print printjob if there's no job printing
+  socket.on('addjob', async (printJob) => {
+    const isPrinting = await prisma.print.count({
+      where: {
+        Status: 'PRINTING'
+      }
+    })
+
+    if (isPrinting == 0) {
+      //update print job status to printing
+      const currentPrintJob = await prisma.print.update({
+        where: { ID: printJob.ID },
+        data: {
+            Status: 'PRINTING'
+        },
+        include: { //includes the model as well as the user attached to it
+          Model: {
+              include: {
+                  User: true
+              }
+          }
+        },
+      })
+
+      socket.broadcast.emit('currentjob', currentPrintJob)
+
+      //update print jobs on the client
+      const updatedPrintJobs = await prisma.print.findMany({
+        where: { 
+          Status: {
+            in: ['PENDING', 'QUEUED']
+          }     
+        },     
+        orderBy: {
+            TimeStamp: 'asc'
+        },
+        include: { //includes the model as well as the user attached to it
+            Model: {
+                include: {
+                    User: true
+                }
+            }
+        },
+        take: 3
+      })
+      //console.log(sendPrintJobs[0].Model)
+      socket.broadcast.emit('printjobs', updatedPrintJobs)
+
+      //print printjob
+      printer.printPrintJob(printJob)
+    }
+  })
+})
+
+httpServer.listen(3000)
