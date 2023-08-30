fdeb60f4f674254547e7d49e39c412da85c1523a
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2023-08-30 15:31:37 -0500

    remove dotenv

--- server/printer.js
+++ server/printer.js
@@ -1,5 +1,3 @@
-const path = require('path')
-require('dotenv').config({ path: path.join(__dirname, '..', '.env') })
 const { SerialPort } = require('serialport')
 const { ReadlineParser} = require('@serialport/parser-readline')
 const fs = require('fs')
--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-const path = require('path')
-require('dotenv').config({ path: path.join(__dirname, '..', '.env') })
 const { createServer } = require("http")
 const { Server } = require("socket.io")
 const printer = require('./printer')
