41aa561847d1dcd3c77d902dd6530a0fcd7a6b2d
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2023-09-06 03:36:42 -0500

    todos

--- app/page.js
+++ app/page.js
@@ -1,16 +1,16 @@
 /*TODO
   [x] set up stl export on button press
-  [ ] implement db
+  [x] implement db
   [x] create one big midi handler js helper file
   [ ] create print queue that waits for user input on the 3d printer screen before printing next print job
-  [ ] buy extra buildplate?
-  [ ] create print queue component that contains print job components with a preview of the model
-  [ ] create slicing process that puts multiple models on the same buildplate (use --marge header in prusaslicer cli)
+  [x] buy extra buildplate?
+  [x] create print queue component that contains print job components with a preview of the model
+  [x] create slicing process that puts multiple models on the same buildplate (use --marge header in prusaslicer cli)
   [ ] 3d print cover for midi controller
-  [ ] stickers - https://thestickybrand.com/products/try-out-our-vinyl-custom-stickers-for-a-limited-time-get-100-2-5custom-vinyl-die-cut-stickers-for-19-00
+  [x] stickers - https://thestickybrand.com/products/try-out-our-vinyl-custom-stickers-for-a-limited-time-get-100-2-5custom-vinyl-die-cut-stickers-for-19-00
   [ ] pretty everything up
   [ ] create rainbow shader for models
-  [ ] create white gridline plane
+  [x] create white gridline plane
   [ ] maybe lighting shader?
   [ ] create ufo model
   [ ] create cactus model
@@ -19,6 +19,7 @@
   [ ] test on tvs
   [ ] calibrate 3d printer
   [ ] generate .ini file for prusaslicer cli
+  [ ] static cmd info panel
 */
 'use client'
 
