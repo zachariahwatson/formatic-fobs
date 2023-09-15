e2b5c132db3809c3c41a9b551d6a71f1d157f573
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2023-09-14 21:55:11 -0500

    added rainbow progress bar

--- app/components/ProgressBar.js
+++ app/components/ProgressBar.js
@@ -4,7 +4,7 @@ export default function ProgressBar({ progress }) {
         <div className="w-full h-full relative overflow-hidden outline-none shadow-md shadow-white border border-white rounded-2xl">
             <div className="rounded-2xl w-full h-full absolute top-0 left-0 z-10" style={{ clipPath: 'inset(0 round .75rem)' }}>
                 <motion.div
-                    className="h-full bg-white rounded-xl absolute top-0 left-0 z-0"
+                    className="h-full rainbow rounded-xl absolute top-0 left-0 z-0"
                     style={{ width: `${progress}%` }}
                     initial={{ width: "0%" }}
                     animate={{ width: `${progress}%` }}
