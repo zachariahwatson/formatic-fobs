f8fc25e7a6e96a504e207f3921d39f87103d9e03
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2023-09-20 16:04:58 -0500

    create settings

---
+++ app/utils/settings.js
@@ -0,0 +1,10 @@
+export const bedCount = 4
+
+//export const exhibitEndTime = new Date("2023-09-22T14:00:00-05:00")
+//export const exhibitEndTime = new Date("2023-09-20T14:10:00-05:00")
+
+function createDate(year, month, day, hour, minute) {
+	return new Date(year, month - 1, day, hour - 1, minute)
+}
+
+export const exhibitEndTime = createDate(2023, 9, 20, 15, 50)
