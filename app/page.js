e4a22023742a743d0deeb60a63cf68f64ad3e475
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2023-09-20 16:03:01 -0500

    added print option

--- app/page.js
+++ app/page.js
@@ -8,31 +8,36 @@
   [x] create slicing process that puts multiple models on the same buildplate (use --marge header in prusaslicer cli)
   [ ] 3d print cover for midi controller
   [x] stickers - https://thestickybrand.com/products/try-out-our-vinyl-custom-stickers-for-a-limited-time-get-100-2-5custom-vinyl-die-cut-stickers-for-19-00
-  [ ] pretty everything up
-  [ ] create rainbow shader for models
+  [x] pretty everything up
+  [x] create rainbow shader for models
   [x] create white gridline plane
-  [ ] maybe lighting shader?
   [ ] create ufo model
-  [ ] create cactus model
-  [ ] create landscape model
   [x] do exhibit simulation test
-  [ ] test on tvs
-  [ ] calibrate 3d printer
-  [ ] generate .ini file for prusaslicer cli
+  [x] calibrate 3d printer
+  [x] generate .ini file for prusaslicer cli
   [ ] static cmd info panel
 */
 "use client"
 
-import { handleTwitterSubmit } from "./actions"
-import { useRouter } from "next/navigation"
-import { motion, AnimatePresence } from "framer-motion"
+import { useRouter, useSearchParams } from "next/navigation"
+import { motion } from "framer-motion"
 import { useEffect, useRef, useState } from "react"
 // import RandomText from './components/RandomText'
 
 export default function Page() {
 	const router = useRouter()
 	const inputRef = useRef()
+	const searchParams = useSearchParams()
 	const [printModel, setPrintModel] = useState(true)
+	const [timesUp, setTimesUp] = useState(false)
+
+	const handleKeyDown = (event) => {
+		if (event.key === "F1") {
+			setPrintModel(true)
+		} else if (event.key === "F2") {
+			setPrintModel(false)
+		}
+	}
 
 	const handleTwitterSubmit = async (e) => {
 		e.preventDefault()
@@ -57,15 +62,19 @@ export default function Page() {
 	}
 
 	useEffect(() => {
-		inputRef.current.focus()
-
-		const handleKeyDown = (event) => {
-			if (event.key === "F1") {
-				setPrintModel(true)
-			} else if (event.key === "F2") {
-				setPrintModel(false)
+		async function fetchTimesUp() {
+			const res = await fetch("http://localhost:3000/timesup").catch((err) => {
+				console.error(err)
+			})
+			if (!res.ok) {
+				console.error("get timesUp error: ", res.status)
 			}
+			const data = await res.json()
+			setTimesUp(data.timesUp)
 		}
+		fetchTimesUp()
+
+		inputRef.current.focus()
 
 		window.addEventListener("keydown", handleKeyDown)
 
@@ -84,7 +93,10 @@ export default function Page() {
 				transition={{ duration: 1 }}
 				className="flex justify-center h-full w-full items-center"
 			>
-				<form onSubmit={handleTwitterSubmit} className="flex flex-col gap-2">
+				<form
+					onSubmit={handleTwitterSubmit}
+					className="flex flex-col gap-2 w-1/3"
+				>
 					<label className="mb-2 text-5xl">
 						<span className="font-n27-extralight">TWITTER_</span>
 						<span className="font-n27-regular">USERNAME:</span>
@@ -95,29 +107,38 @@ export default function Page() {
 						className={`rounded-2xl p-2 bg-black text-3xl outline-none shadow-lg shadow-white border border-white font-n27-regular lowercase`}
 						ref={inputRef}
 					/>
-					<label className="mt-6 text-3xl text-center">
-						<span className="font-n27-extralight">PRINT MODEL?</span>
-						<span className="flex justify-evenly">
-							<p
-								className={
-									printModel
-										? "font-n27-regular"
-										: "font-n27-extralight text-neutral-500"
-								}
-							>
-								F1 -&gt; YES
-							</p>
-							<p
-								className={
-									printModel
-										? "font-n27-extralight text-neutral-500"
-										: "font-n27-regular"
-								}
-							>
-								F2 -&gt; NO
-							</p>
-						</span>
-					</label>
+					{(timesUp === false && (
+						<label className="mt-6 text-3xl text-center">
+							<span className="font-n27-extralight">PRINT MODEL?</span>
+							<span className="flex justify-evenly">
+								<p
+									className={
+										printModel
+											? "font-n27-regular"
+											: "font-n27-extralight text-neutral-500"
+									}
+								>
+									F1 -&gt; YES
+								</p>
+								<p
+									className={
+										printModel
+											? "font-n27-extralight text-neutral-500"
+											: "font-n27-regular"
+									}
+								>
+									F2 -&gt; NO
+								</p>
+							</span>
+						</label>
+					)) || (
+						<label className="mt-6 text-3xl text-center">
+							<span className="font-n27-extralight">
+								FYI: THE QUEUE IS FULL AND YOUR MODEL WILL NOT BE PRINTED BY THE
+								END OF THIS ACTIVATION (sorry)
+							</span>
+						</label>
+					)}
 				</form>
 			</motion.div>
 		</>
