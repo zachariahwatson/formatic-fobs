455e341b486b674050421dd213223bc35af87b3f
zachariahwatson <watsonzachariah@gmail.com>
2024-10-29 02:13:50 -0500

    tweak logo padding

--- app/layout.js
+++ app/layout.js
@@ -27,10 +27,10 @@ export default function RootLayout({ children }) {
 					>
 						<div className="w-full h-28 bg-gradient-to-b from-black from-0% to-transparent absolute top-0 left-0 z-10 rounded-3xl">
 							<Link href="/">
-								<div className="flex flex-row justify-start pl-6">
+								<div className="flex flex-row justify-start pl-3">
 									<Image className="object-contain" src={formfinityLogo} width={64} height={64} alt="formfinity logo" />
 
-									<p className="text-7xl px-6 pt-4 pb-2">
+									<p className="text-7xl px-3 pt-4 pb-2">
 										<span className="font-copula">formatic</span>
 										<span className="font-waffold">FOBS</span>
 									</p>
